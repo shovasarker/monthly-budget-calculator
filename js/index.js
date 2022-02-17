ed31a5268043ccc34ff54499680721bde8546ff3
ShovaSarker <shovasarker01@gmail.com>
2022-02-17 07:34:29 +0600

    Added Comments

--- js/index.js
+++ js/index.js
@@ -1,12 +1,16 @@
 let incomeValue = 0
+
+// function for Displaying Error Message
 function displayErrorMessage(message) {
   const displayContainer = document.getElementById('error-display-container')
   const errorDisplay = document.getElementById('error-display')
   errorDisplay.innerText = message
+  // adding and removing tailwind-css class to the error display container
   displayContainer.classList.remove('hidden')
   displayContainer.classList.add('flex')
 }
 
+// function For Hiding Error Message
 function hideErrorDisplay() {
   const displayContainer = document.getElementById('error-display-container')
   if (!displayContainer.classList.contains('hidden')) {
@@ -15,17 +19,22 @@ function hideErrorDisplay() {
   }
 }
 
+// Function for Getting Input Value and Some Error Checking
 function getInputValue(inputId) {
   const input = document.getElementById(inputId)
   const inputValueText = input.value
   const inputValue = parseInt(inputValueText)
   const displayName = inputId.split('-').join(' ').toUpperCase()
+  // checking if input value is a number or not
   if (isNaN(inputValue)) {
+    //Creating Error Message And Displaying it
     const message = 'Please Enter a Number in the ' + displayName + ' field'
     displayErrorMessage(message)
     return -1
   }
+  //checking for a positive number
   if (inputValue < 0) {
+    //Creating Error Message And Displaying it
     const message =
       'Please Enter a Positive Number in the ' + displayName + ' field'
     displayErrorMessage(message)
@@ -35,26 +44,28 @@ function getInputValue(inputId) {
   return inputValue
 }
 
+// function for Displaying Calculated Value
 function displayValue(displayId, value) {
   const display = document.getElementById(displayId)
   display.innerText = value
 }
 
+// function for getting calculated displayed value
 function getDisplayedValue(displayId) {
-  debugger
   const display = document.getElementById(displayId)
   const displayValue = parseInt(display.innerText)
   if (isNaN(displayValue)) {
+    //Creating Error Message And Displaying it
     const message = 'Please Calculate Balance First!'
     displayErrorMessage(message)
     return -1
   }
-
-  console.log(displayValue)
   return displayValue
 }
 
+// function for calculate Balance
 function calculate() {
+  //Getting Input Values
   incomeValue = getInputValue('income-input')
   if (incomeValue === -1) return
 
@@ -67,36 +78,54 @@ function calculate() {
   const clothesValue = getInputValue('clothes-input')
   if (clothesValue === -1) return
 
+  //Calculating total Expenses
   const totalExpenses = foodValue + rentValue + clothesValue
 
+  //comparing totalExpense and incomevalue
   if (totalExpenses > incomeValue) {
+    //Creating Error Message And Displaying it
     const message = "You don't have enough money for your Expenses."
     displayErrorMessage(message)
     return
   }
-
-  displayValue('total-expense-display', totalExpenses)
-
+  //Calculating balance
   const balance = incomeValue - totalExpenses
+
+  //displaying calculated value
+  displayValue('total-expense-display', totalExpenses)
   displayValue('balance-display', balance)
+  //hiding error display
   hideErrorDisplay()
 }
 
+//function for calculate savings
 function savings() {
+  //getting save-input
   const savingsValue = getInputValue('save-input')
   if (savingsValue === -1) return
 
+  //calculating savings
   const savings = (incomeValue * savingsValue) / 100
+
+  //getting balance value from balance-display
   const balance = getDisplayedValue('balance-display')
   if (balance === -1) return
 
+  //checking if savings is bigger than balance or not
   if (savings > balance) {
+    //Creating Error Message And Displaying it
     const message = "You don't have enough money for your savings."
     displayErrorMessage(message)
     return
   }
+
+  //calculating remaining balance
   const remainingBalance = balance - savings
+
+  //displaying calculated values
   displayValue('saving-amount-display', savings)
   displayValue('remaining-balance-display', remainingBalance)
+
+  //hiding Error display
   hideErrorDisplay()
 }
